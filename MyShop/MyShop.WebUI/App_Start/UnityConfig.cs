6b61ce0692c355443a30a9c44071f1a461f4156b
mbanakova <mbanakova@yandex.ru>
2022-08-29 20:19:09 +0300

    creating orders, broke the tests

--- MyShop/MyShop.WebUI/App_Start/UnityConfig.cs
+++ MyShop/MyShop.WebUI/App_Start/UnityConfig.cs
@@ -51,8 +51,9 @@ namespace MyShop.WebUI
             container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
             container.RegisterType<IRepository<Cart>, SQLRepository<Cart>>();
             container.RegisterType<IRepository<CartItem>, SQLRepository<CartItem>>();
-            container.RegisterType<IRepository<Customer>, SQLRepository<Customer>>();
+            container.RegisterType<IRepository<Order>, SQLRepository<Order>>();
             container.RegisterType<ICartService, CartService>();
+            container.RegisterType<IOrderService, OrderService>();
         }
     }
 }
